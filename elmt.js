03fc0e32c9f3fde2b89f8e7bb575c416496d5543
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-25 15:19:52 -0600

    Update elmt.js

--- elmt.js
+++ elmt.js
@@ -1,1 +1,19 @@
+var attrib = ["accesskey", "class", "contenteditable", "dir", "draggable",
+              "dropzone", "hidden", "id", "lang", "spellcheck", "tabindex",
+              "title", "translate"];
 
+var others = {"input": ["
+
+function create(name, text, attr, ls) {
+    s = "<"+name;
+    for(var a of attr.keys()) {
+        if(ls.includes(a.toLower())) {
+            s += ` ${a}="${attr.get(a)}"`;
+        }
+    }
+    s += ">"+text+"</"+name+">";
+    return s;
+
+function INPUT(text, attr) {
+    ls = attrib + ["accept"];
+    return create("input", text, attr, ls);
