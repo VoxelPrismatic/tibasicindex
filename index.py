a637624f80c22344895ac0b1c6188b10924ffc79
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-24 18:32:47 -0600

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,15 @@
 import os
+global dirs
+dirs = []
+def grab_dirs(lvl = "."):
+    global dirs
+    for filename in os.listdir():
+        if filename.endswith(".md"):
+            dirs.append(lvl+"/"+filename)
+        elif "." not in filename:
+            try:
+                os.listdir(lvl+"/"+filename)
+                grab_dirs(lvl+"/"+filename)
+            except NotADirectoryError:
+                pass
+print(dirs)
