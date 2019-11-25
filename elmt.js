1c692a1d6e6c6cab910a674f8f09e49cdc9d463b
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-25 15:31:05 -0600

    Update elmt.js

--- elmt.js
+++ elmt.js
@@ -1,19 +1,25 @@
-var attrib = ["accesskey", "class", "contenteditable", "dir", "draggable",
-              "dropzone", "hidden", "id", "lang", "spellcheck", "tabindex",
-              "title", "translate"];
+var attrib = new Map();
+attrib["global"] = ["accesskey", "class", "contenteditable", "dir", "draggable",
+                    "dropzone", "hidden", "id", "lang", "spellcheck", "tabindex",
+                    "title", "translate"];
 
-var others = {"input": ["
+attrib["a"] = ["download", "href", "hreflang", "media", "ping", "referrerpolicy",
+               "rel", "target", "type"];
+attrib["abbr"] = [];
 
-function create(name, text, attr, ls) {
+//attrib["
+
+function create(name, text, attr) {
     s = "<"+name;
+    ls = attrib[name]+attrib["global"]
     for(var a of attr.keys()) {
         if(ls.includes(a.toLower())) {
             s += ` ${a}="${attr.get(a)}"`;
-        }
+        } //Ignore bad attributes, 
     }
     s += ">"+text+"</"+name+">";
     return s;
 
 function INPUT(text, attr) {
-    ls = attrib + ["accept"];
-    return create("input", text, attr, ls);
+    return create("input", text, attr);
+}
