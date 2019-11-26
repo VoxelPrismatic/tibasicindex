8a4e8bce59476ec8f00ec10fe0f83f838e8f00c5
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-26 17:48:39 -0600

    Create read.js

---
+++ script/read.js
@@ -0,0 +1,10 @@
+function read(filename) {
+    var f = new XMLHttpRequest()
+    f.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200)
+            find("file").innerHTML = f.responseText;
+    }
+    f.open("GET", filename, false);
+    f.send();
+    return find("file").innerHTML;
+}
