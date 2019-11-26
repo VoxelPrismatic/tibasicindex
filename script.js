0a54d960d2bd6c0d7fa3923a8c87b647db2bb13c
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-26 14:46:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function find_in(thing, ids) {
     ls = [];
     for(var id of ids.split(" ")) {
-        if(id.startsWith(".")) {
+        if(id.startsWith("."))
             ls.push(...thing.getElementsByClassName(id.slice(1)));
-        } else if(id.startsWith(">")) {
+        else if(id.startsWith(">"))
             ls.push(...thing.getElementsByTagName(id.slice(1)));
-        } else if(id.startsWith(":")) {
+        else if(id.startsWith(":"))
             ls.push(...thing.getElementsByName(id.slice(1)));
-        } else {
+        else
             ls.push(thing.getElementById(id));
-        }
     }
     return ls;
-} function find(ids) {
+} 
+
+function find(ids) {
     return find_in(document, ids);
 }
 
 function read(filename) {
     var f = new XMLHttpRequest()
     f.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200)
             find("file").innerHTML = f.responseText;
-        }
     }
     f.open("GET", filename, false);
     f.send();
@@ -31,9 +31,8 @@ function jump(elem) {
     id = elem.id.slice(5);
     find(id).scrollIntoView();
     var things = find("sect").children;
-    for(var thing of things) {
+    for(var thing of things)
         thing.className = "lnk";
-    }
     elem.className = "alnk";
 }
 function load(fil) {
@@ -46,13 +45,12 @@ function load(fil) {
         ["&", "&amp;"],
         [">", "&gt;"],
         ["<", "&lt;"]
-    ]
+    ];
     for(var ls of chars) {
         k = ls[0];
         v = ls[1];
-        while(txt.includes(k)) {
+        while(txt.includes(k))
             txt = txt.replace(k, v);
-        }
     }
     txt = txt.replace(/\\U([A-Fa-f0-9]{16})/gm, "\\u{$1}");
     var md = "<div id='top'></div>";
@@ -82,16 +80,14 @@ function load(fil) {
             find("sect").innerHTML += `<div class="lnk" id="${line}" onclick="jump(this);">#${line.slice(5)}</div>`;
             continue;
         }
-        for(var rep of reps) {
+        for(var rep of reps)
             line = line.replace(rep[0], rep[1]);
-        }
-        if(line.endsWith("<br>")) {
+        if(line.endsWith("<br>"))
             md += line;
-        } else if(line.endsWith("§")) {
+        else if(line.endsWith("§"))
             md += line.slice(0, -1);
-        } else {
-            md += line+"\n"
-        }
+        else
+            md += line+"\n";
     }
     find("page").innerHTML = md.replace(/\n/gm, "<br>")
     fil = fil.replace("index.txt", "").replace(".txt", ".py")
@@ -99,23 +95,22 @@ function load(fil) {
 function docs(elem) {
     load(elem.id);
     var things = find("nav").children;
-    for(var thing of things) {
+    for(var thing of things)
         thing.className = "lnk";
-    }
     elem.className = "alnk";
     jump("SECT_top");
 }
 function grab_dirs(lvl) {
     var dirs = [];
-    var lines = read(lvl+"/dir.txt").split("\n")
+    var lines = read(lvl+"/dir.txt").split("\n");
     for(var line of lines) {
         if(line.endsWith(".txt")) {
-            lnk = lvl+"/"+line
-            var smol = lnk.slice(5)
+            lnk = lvl+"/"+line;
+            var smol = lnk.slice(5);
             dirs.push(lnk);
             if(lnk.endsWith("index.txt")) {
-                lnk = lnk.replace("index.txt", "")
-                var smol = lnk.slice(5)
+                lnk = lnk.replace("index.txt", "");
+                var smol = lnk.slice(5);
             } else {
                 nam = smol.slice(1)
                 fil = lvl+"/"+line
@@ -124,9 +119,8 @@ function grab_dirs(lvl) {
             }
         } else if(line != "") {
             try {
-                for(var name of grab_dirs(lvl+"/"+line)) {
+                for(var name of grab_dirs(lvl+"/"+line))
                     dirs.push(name);
-                }
             } catch(err) {}
         }
     }
