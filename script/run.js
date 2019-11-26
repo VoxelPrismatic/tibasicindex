c97638c8677f7afbf8f4cda25975152558fb745e
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-26 17:51:34 -0600

    Create run.js

---
+++ script/run.js
@@ -0,0 +1,31 @@
+dirs = grab_dirs("./doc");
+url = document.URL;
+if(url.includes("?")) {
+    var thing = url.split("?")[1].split("#")[0];
+    if(thing.startsWith("./doc"))
+        thing = thing.slice(5);
+    if(thing.startsWith("/"))
+        thing = thing.slice(1)
+    var fil = "./doc/"+thing.replace(/\.txt/g, "&").
+    replace(/\./g, "/").replace(/\&/g, ".txt");
+} else {
+    var fil = "./doc/index.txt";
+} if (!(fil.endsWith(".txt"))) {
+    fil += ".txt";
+} if (!(dirs.includes(fil))) {
+    fil += "index.txt";
+} if (!(dirs.includes(fil))) {
+    find("page").innerHTML = `<div class="warn">404 ] File not found</div>`;
+} else {
+    load(fil);
+}
+if(url.includes("#")) {
+    url = url.replace("##", "#")
+    var sec = url.split("#")[1].split("?")[0];
+    if(!(sec.startsWith("SECT_"))) {
+        sec = "SECT_"+sec;
+    }
+    jump(sec);
+} else {
+    jump("SECT_top");
+}
