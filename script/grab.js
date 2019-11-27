81ae7f7685f4042f5bff718e68f7e44c49e0b42d
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-27 14:02:17 -0600

    Update grab.js

--- script/grab.js
+++ script/grab.js
@@ -3,14 +3,14 @@ function grab_dirs(lvl) {
     var lines = read(lvl+"/dir.txt").split("\n");
     for(var line of lines) {
         if(line.endsWith(".txt")) {
-            lnk = lvl.replace("/prizmatic.docs", "..");+"/"+line;
-            var smol = lnk.slice(6);
+            lnk = line+"/"+line;
+            var smol = lnk.slice(19);
             dirs.push(lnk);
             if(lnk.endsWith("index.txt")) {
                 lnk = lnk.replace("index.txt", "");
-                var smol = lnk.slice(6);
+                var smol = lnk.slice(19);
             } else {
-                nam = smol.slice(2)
+                nam = smol.slice(15)
                 fil = lvl+"/"+line
                 find("nav").innerHTML +=
                     `<div class="lnk" id="${fil}" onclick="docs(this);">${smol}</div>`;
