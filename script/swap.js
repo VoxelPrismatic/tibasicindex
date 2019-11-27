199656fc3fdf28bf18a843427f9f363b2118e674
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-11-26 18:55:43 -0600

    Update swap.js

--- script/swap.js
+++ script/swap.js
@@ -3,14 +3,16 @@ function jump(elem) {
     find(id).scrollIntoView();
     var things = find("sect").children;
     for(var thing of things)
-        thing.className = "lnk";
+        if(thing.className == "alnk")
+            thing.className = "lnk";
     elem.className = "alnk";
 }
 function docs(elem) {
     load(elem.id);
     var things = find("nav").children;
     for(var thing of things)
-        thing.className = "lnk";
+        if(thing.className == "alnk")
+            thing.className = "lnk";
     elem.className = "alnk";
     jump("SECT_top");
 }
