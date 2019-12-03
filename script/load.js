78773e87d735312ec4b36a37e7d1a3b36153e6a9
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 15:41:33 -0600

    Update load.js

--- script/load.js
+++ script/load.js
@@ -22,41 +22,24 @@ function load(fil) {
 }
 
 function maybeload(url) {
-    if(url.includes("?")) {
-        var thing = url.split("?")[1].split("#")[0];
-        if(thing.startsWith("./doc"))
-            thing = thing.slice(5);
-        if(thing.startsWith("/"))
-            thing = thing.slice(1);
-        if(thing.endsWith(".txt")) {
-            thing = thing.slice(0, -3);
-            thing = thing.replace(/\./g, "/");
-            thing += ".txt";
-        } else {
-            thing = thing.replace(/\./g, "/");
-            thing += ".txt";
-        }
-        var fil = "/prizmatic.docs/doc/"+thing;
-    } else {
-        var fil = "/prizmatic.docs/doc/index.txt";
-    } if (!(dirs.includes(fil))) {
-        fil += "index.txt";
-    } if (!(dirs.includes(fil))) {
+    if (!(url.startsWith("/prizmatic.docs/doc/")));
+          url = "/prizmatic.docs/doc/" + url
+    url = url.replace(/\/\/gm, "/");
+    if(!(url.endsWith(".txt")))
+       url += "index.txt;
+    if (!(dirs.includes(fil))))
         find("page").innerHTML = `<div class="warn">404 ] File not found</div>`;
-        find("page").innerHTML = "This search works by finding all documents upon the page loading, " +
-                                 "then checks to see if the requested filename is in the directory list. " +
-                                 "If you think this file should exist, try refreshing the page."
-    } else {
+    else 
         docs(fil);
-    }
     if(url.includes("#")) {
-        url = url.replace("##", "#")
-        var sec = url.split("#")[1].split("?")[0];
-        if(!(sec.startsWith("JUMP_"))) {
+        var sec = url.split("#")[1].split("?")[0].split("&")[0];
+        if(!(sec.startsWith("JUMP_")))
             sec = "JUMP_"+sec;
-        }
         jump(sec);
-    } else {
+    } else
         jump("JUMP_top");
+    if(url.includes("&")) {
+        var sec = url.split("#")[1].split("?")[0].split("&")[0];
+        find("page").innerHTML = find("page").innerHTML.replace(RegExp(sec, "gm"), `<div class="find">${sec}</div>`);
     }
 }
