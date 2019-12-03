4a222c7d10c9078f6c06241f71c751df8d34e40b
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 15:43:05 -0600

    Update run.js

--- script/run.js
+++ script/run.js
@@ -1,2 +1,40 @@
 dirs = grab_dirs("/prizmatic.docs/doc"); // ../doc doesn't actually work for some reason
-maybeload(document.URL.split("github.io/").slice(-1)[0]);
+url = document.URL;
+if(url.includes("?")) {
+    var thing = url.split("?")[1].split("#")[0];
+    if(thing.startsWith("./doc"))
+        thing = thing.slice(5);
+    if(thing.startsWith("/"))
+        thing = thing.slice(1);
+    if(thing.endsWith(".txt")) {
+        thing = thing.slice(0, -3);
+        thing = thing.replace(/\./g, "/");
+        thing += ".txt";
+    } else {
+        thing = thing.replace(/\./g, "/");
+        thing += ".txt";
+    }
+    var fil = "/prizmatic.docs/doc/"+thing;
+} else {
+    var fil = "/prizmatic.docs/doc/index.txt";
+}if (!(dirs.includes(fil))) {
+    fil += "index.txt";
+} if (!(dirs.includes(fil))) {
+    find("page").innerHTML = `<div class="warn">404 ] File not found</div>`;
+} else {
+    load(fil);
+}
+if(url.includes("#")) {
+    url = url.replace("##", "#")
+    var sec = url.split("#")[1].split("?")[0];
+    if(!(sec.startsWith("JUMP_"))) {
+        sec = "JUMP_"+sec;
+    }
+    jump(sec);
+} else {
+    jump("JUMP_top");
+}
+if(url.includes("&")) {
+    var sec = url.split("#")[1].split("?")[0].split("&")[0];
+    find("page").innerHTML = find("page").innerHTML.replace(RegExp(sec, "gm"), `<div class="find">${sec}</div>`);
+}
