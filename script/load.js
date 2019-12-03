2527e28c4bf7743d8fe11c8af563a4ac0ec4cd2d
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-02 22:16:07 -0600

    Update load.js

--- script/load.js
+++ script/load.js
@@ -2,20 +2,15 @@ function load(fil) {
     find("sect").innerHTML = `<div class="lnk" id="SECT_top" onclick="jump(this);">#top</div>`;
     find("page").innerHTML = "WAIT... [LOADING FILE]";
     find(fil).className = "alnk";
-    console.log("Reading file" + fil);
     txt = read(fil);
-    console.log("RegEx Bits");
     chars = [
-        [/\\ *\\n/gm, ""],
-        [/\\U([A-Fa-f0-9]{16})/gm, "\\u{$1}"],
-        [/ /gm, "\u200b \u200b"],
+        [/\\ *\\n/gm, ""], //New line escape
     ];
     for(var ls of chars)
         txt = txt.replace(ls[0], ls[1]);
     var md = "<div id='top'></div>";
     console.log("RegEx Styles");
     for(var line of txt.split("\n")) {
-        console.log(line);
         if(line.startsWith("SECT_")) {
             find("sect").innerHTML += `<div class="lnk" id="${line}" onclick="jump(this);">#${line.slice(5)}</div>`;
             continue;
