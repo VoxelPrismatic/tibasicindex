2256ed217fa335840bf8c15a27751c3934a16d2f
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 12:39:18 -0600

    Update load.js

--- script/load.js
+++ script/load.js
@@ -19,6 +19,4 @@ function load(fil) {
             md += line+"\n";
     }
     find("page").innerHTML = md.replace(/\n/gm, "<br>");
-    find("edit_page").href = `https://github.com/VoxelPrismatic/prizmatic.docs/edit/master/doc/${fil.split("doc/")[-1]}`;
-    find("edit_page").innerHTML = fil.split("doc")[-1].replace("index.txt", "").toUpper();
 }
