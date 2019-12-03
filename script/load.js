506a1e717956f9c2f973e894dfe654dd37647eda
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 13:33:06 -0600

    Update load.js

--- script/load.js
+++ script/load.js
@@ -20,3 +20,43 @@ function load(fil) {
     }
     find("page").innerHTML = md.replace(/\n/gm, "<br>");
 }
+
+function maybeload(url) {
+    if(url.includes("?")) {
+        var thing = url.split("?")[1].split("#")[0];
+        if(thing.startsWith("./doc"))
+            thing = thing.slice(5);
+        if(thing.startsWith("/"))
+            thing = thing.slice(1);
+        if(thing.endsWith(".txt")) {
+            thing = thing.slice(0, -3);
+            thing = thing.replace(/\./g, "/");
+            thing += ".txt";
+        } else {
+            thing = thing.replace(/\./g, "/");
+            thing += ".txt";
+        }
+        var fil = "/prizmatic.docs/doc/"+thing;
+    } else {
+        var fil = "/prizmatic.docs/doc/index.txt";
+    } if (!(dirs.includes(fil))) {
+        fil += "index.txt";
+    } if (!(dirs.includes(fil))) {
+        find("page").innerHTML = `<div class="warn">404 ] File not found</div>`;
+        find("page").innerHTML = "This search works by finding all documents upon the page loading, " +
+                                 "then checks to see if the requested filename is in the directory list. " +
+                                 "If you think this file should exist, try refreshing the page."
+    } else {
+        docs(fil);
+    }
+    if(url.includes("#")) {
+        url = url.replace("##", "#")
+        var sec = url.split("#")[1].split("?")[0];
+        if(!(sec.startsWith("JUMP_"))) {
+            sec = "JUMP_"+sec;
+        }
+        jump(sec);
+    } else {
+        jump("JUMP_top");
+    }
+}
