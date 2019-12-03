d8545570a351088536e41c61b5f825bb49c9bd30
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-02 21:52:30 -0600

    Update load.js

--- script/load.js
+++ script/load.js
@@ -17,36 +17,13 @@ function load(fil) {
         txt = txt.replace(ls[0], ls[1]);
     var md = "<div id='top'></div>";
     console.log("RegEx Styles");
-    reps = [
-        [/^(\#+)(.+)$/gm, "<div class='head'>$1$2</div>"],
-        [/\[(.+)\]\((.+)\)/gm, "<a href='$2'>$1</a>"],
-        [/\[\[(.+)\]\]\((.+)\)/gm, "<a href='$2'><div class='lnk'>$1</div></a>"],
-        [/\%(.+)\%/gm, "<b>$1</b>"],
-        [/\*(.+)\*/gm, "<i>$1</i>"],
-        [/\_(.+)\_/gm, "<u>$1</u>"],
-        [/\~(.+)\~/gm, "<s>$1</s>"],
-        [/\`(.+)\`/gm, `<span class="code">$1</span>`],
-        [/^WARN---$/gm, "<div class='warn'><b>WARNING ---</b><br>"],
-        [/^NOTE---$/gm, "<div class='note'><b>NOTICE ---</b><br>"],
-        [/^NEW---$/gm, "<div class='new'><b>NEW ---</b><br>"],
-        [/^INFO---$/gm, "<div class='info'><b>INFO ---</b><br>"],
-        [/^EX---$/gm, "<div class='exc'><b>EXAMPLES ---</b><br>"],
-        [/^CODE---$/gm, "<div class='code'>§"],
-        [/^COMMENT---$/gm, "<div class='comblock'><b>OTHER INFO ---</b><br>"],
-        [/^ *(\w+) *---{3,}/gm, "<b>$1 ---</b>"],
-        [/^---$/gm, "</div>"],
-        [/^ *\|(.*)$/gm, "<div class='com'>&gt; $1</div>"],
-        [/\{\{(\w+)\}\}([\w\d]+) /gm, "<span class='$1'>$2</span>"],
-        [/--([\w]+)--/gm, "<div id='$1'></div>"],
-    ];
     for(var line of txt.split("\n")) {
         console.log(line);
         if(line.startsWith("SECT_")) {
             find("sect").innerHTML += `<div class="lnk" id="${line}" onclick="jump(this);">#${line.slice(5)}</div>`;
             continue;
         }
-        for(var rep of reps)
-            line = line.replace(rep[0], rep[1]);
+        line = mark(line);
         if(line.endsWith("<br>"))
             md += line;
         else if(line.endsWith("§"))
