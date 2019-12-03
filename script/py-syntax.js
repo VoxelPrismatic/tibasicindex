f5daa3bdba6551944878a0f5ee7fe742115de619
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 09:38:08 -0600

    Update py-syntax.js

--- script/py-syntax.js
+++ script/py-syntax.js
@@ -1,7 +1,32 @@
+cls = [
+    "int", "float", "dict", "list",
+    "tuple", "set", "bool", "None",
+    "frozenset"
+];
+
+vars = [
+];
+
+kw = [
+    "True", "False", "def", "import",
+    "from", "yield", "class", "if",
+    "elif", "else", "for", "while",
+    "async", "await", "with", "as",
+    "in", "not", "and", "or", "is"
+]
+ops = [
+    "+", "-", "*", "^", "|", "~", ">", 
+    "<", "%", "=", "/", "@", "&", "[",
+    "]", "(", ")", "{", "}", "'", '"',
+    ":", ";", ",", "."
+];
+    
+
 py_regex = [
     [/( *)def ([\w\d_]+)/gm, `$1<span class="kw">def</span> <span class="fn">$2</span>`],
     [/( *)class ([\w\d_]+)/gm, `$1<span class="kw">class</span> <span class="cls">$2</span>`],
     [/([fFrRuUbB]?)(['"])(.+?)$2/gm, `<span class="str">$1$2$3$2</span>`],
-]
+    [/([fFrRuUbB]?)('''|""")((.|\n)+)$2/gm, `<span class="str">$1$2$3$2</span>`],
+];
 
 function py_mark(st) {
