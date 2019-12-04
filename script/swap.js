e18661901646f22340d025f21f15ed9bb1de9909
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-03 19:53:42 -0600

    Update swap.js

--- script/swap.js
+++ script/swap.js
@@ -1,10 +1,10 @@
 function jump(elem) {
+    console.log("Jumping");
     try {
         id = elem.id.split("JUMP_").slice(-1)[0];
     } catch {
         id = elem.split("JUMP_").slice(-1)[0];
     }
-    console.log(id);
     find(id).scrollIntoView();
     var things = find("sect").children;
     for(var thing of things)
@@ -13,6 +13,7 @@ function jump(elem) {
     find("JUMP_"+id).className = "lnk sel";
 }
 function docs(elem) {
+    console.log("Swapping");
     try {
         id = elem.id;
     } catch(err) {
@@ -34,4 +35,22 @@ function docs(elem) {
         `https://github.com/VoxelPrismatic/prizmatic.docs/edit/master/doc/${id.split("doc/").slice(-1)[0]}`;
     find("edit_page").innerHTML = "/"+id.split("doc/").slice(-1)[0].replace("index.txt", "").toUpper();
     jump("JUMP_top");
+}
+function finder(thing) {
+    var ls = [];
+    for(var file of dirs) {
+        try {
+            if(find("RAW_"+file).innerHTML.includes(thing))
+                ls.push(file);
+        } catch(err) {
+            txt = mayberead(file)
+            find("cached-pages").innerHTML += `<div id="RAW_${file}" class="invis">${txt}</div>`
+            if(txt.includes(thing))
+                ls.push(file);
+        }
+    }
+    st = "";
+    for(var file of ls)
+        st += `<div onclick="maybeload('${file}&${thing}')" class="exc">${file}</div>`;
+    find("page").innerHTML = st;
 }
