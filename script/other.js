d1383d7fd73b5d3b2cbdd7b15bbee1841c33120f
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-05 14:12:09 -0600

    Create other.js

---
+++ script/other.js
@@ -0,0 +1,10 @@
+function btn(elem, id) {
+    btns = find(".tab");
+    for(el of btns)
+        el.className = "tab";
+    elem.className = "tab tabbed";
+    pages = find(".page");
+    for(el of pages)
+        el.style.display = "none";
+    find(id).style.display = "block";
+}
