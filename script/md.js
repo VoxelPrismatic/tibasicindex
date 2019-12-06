a530c7c85f009c876ae586a7814bba5962714c91
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 15:09:41 -0600

    Update md.js

--- script/md.js
+++ script/md.js
@@ -5,12 +5,12 @@ var regex = [
     [/\\U([A-Fa-f0-9]{16})/gm, "\\u{$1}"],
     [/\\u([A-Fa-f0-9]{4})/gm, "\\u{$1}"],
     
-    [/^\#\] +(.+)$/gm, "<div class='head1'>#] $1</div>§"]
-    [/^\~\] +(.+)$/gm, "<div class='head2'>~] $1</div>§"],
-    [/^\+\] +(.+)$/gm, "<div class='head3'>+] $1</div>§"],
-    [/^\-\] +(.+)$/gm, "<div class='head4'>-] $1</div>§"],
-    [/^\$\] +(.+)$/gm, "<div class='head5'>$] $1</div>§"],
-    [/^\%\] +(.+)$/gm, "<div class='head6'>%] $1</div>§"],
+    [/^\#\] +(.+)$/gm, "<div class='head1'>#] $1</div></br>"]
+    [/^\~\] +(.+)$/gm, "<div class='head2'>~] $1</div></br>"],
+    [/^\+\] +(.+)$/gm, "<div class='head3'>+] $1</div></br>"],
+    [/^\-\] +(.+)$/gm, "<div class='head4'>-] $1</div></br>"],
+    [/^\$\] +(.+)$/gm, "<div class='head5'>$] $1</div></br>"],
+    [/^\%\] +(.+)$/gm, "<div class='head6'>%] $1</div></br>"],
     
     [/\[(.+?)\]\((.+?)\)/gm, "<a href='$2'>$1</a>"],
     [/\[\[(.+?)\]\]\((.+?)\)/gm, "<a href='$2'><div class='lnk'>$1</div></a>"],
@@ -35,19 +35,19 @@ var regex = [
     [/([^\\])\|(.+?)\|/gm, `$1<span class="hide" onclick="this.classList.toggle('unhide');">$2</span>`],
     [/([^\\])\:\: (.+)$/gm, `$1<span class="md-com">\u200b \u200b$2</span>`],
     
-    [/^WARN---$/gm, "<div class='warn'><b>WARNING ---</b>"],
-    [/^NOTE---$/gm, "<div class='note'><b>NOTICE ---</b>"],
-    [/^NEW---$/gm, "<div class='new'><b>NEW ---</b>"],
-    [/^INFO---$/gm, "<div class='info'><b>INFO ---</b>"],
-    [/^EX---$/gm, "<div class='exc'><b>EXAMPLES ---</b>"],
-    [/^CODE---$/gm, "<div class='code'>§"],
-    [/^COMMENT---$/gm, "<div class='comblock'><b>OTHER INFO ---</b>"],
-    [/^ *(\w+) *-{3,}/gm, "<b>$1 ---</b>"],
-    [/^---$/gm, "</div>§"],
+    [/^WARN---$/gm, "<div class='warn'><b>WARNING ---</b><br>"],
+    [/^NOTE---$/gm, "<div class='note'><b>NOTICE ---</b><br>"],
+    [/^NEW---$/gm, "<div class='new'><b>NEW ---</b><br>"],
+    [/^INFO---$/gm, "<div class='info'><b>INFO ---</b><br>"],
+    [/^EX---$/gm, "<div class='exc'><b>EXAMPLES ---</b><br>"],
+    [/^CODE---$/gm, "<div class='code'></br>"],
+    [/^COMMENT---$/gm, "<div class='comblock'><b>OTHER INFO ---</b><br>"],
+    [/^ *(\w+) *-{3,}/gm, "<b>$1 ---</b><br>"],
+    [/^---$/gm, "</div></br>"],
     
     [/\{\{(\w+?)\}\}([\w\d]+?) /gm, "<span class='$1'>$2 </span>"],
-    [/^--([\w\d_.-]+)--$/gm, "<div id='$1'></div>§"],
-    [/\\ *$/gm, "§"], //New line escape
+    [/^--([\w\d_.-]+)--$/gm, "<div id='$1'></div></br>"],
+    [/\\ *$/gm, "</br>"], //New line escape
     
     [/\\u\{([a-fA-F0-9]+)\}/gm, function(m, p1) {return String.fromCharCode("0x"+p1);}],
 ];
