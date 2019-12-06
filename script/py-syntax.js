87f795b1dde21f50cf928ce2a4cc5695b519f2a8
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 08:13:09 -0600

    Update py-syntax.js

--- script/py-syntax.js
+++ script/py-syntax.js
@@ -20,7 +20,7 @@ py_regex = [
     [
         /([\[\]\{\}\%\^\*\+\=\|\\\~\!\.\,\<\>\:\;\-\(\)\/\&])/gm,
         `<span class="op">$1</span>`
-    [
+    ], [
         /([fFrRuUbB]?)(['"])(.+?)\g<2>/gm,
         function(m, a, b, c) {
             return `<span class="str">${a}${b}${c.split('').join('\u200b')}${b}</span>`;
