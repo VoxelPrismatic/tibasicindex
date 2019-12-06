8d976eed9fcf700cccde0dd08cd6d75e15a10759
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 07:38:02 -0600

    Update py-syntax.js

--- script/py-syntax.js
+++ script/py-syntax.js
@@ -22,7 +22,7 @@ py_regex = [
         `$1<span class="kw">def</span> <span class="fn">$2</span>`
     ], [
         /( *)class ([\w\d_]+)/gm,
-        `${m[1]}<span class="kw">class</span> <span class="cls">${m[2]}</span>`
+        `$1<span class="kw">class</span> <span class="cls">$2</span>`
     ], [
         /([fFrRuUbB]?)(['"])(.+?)$2/gm,
         `<span class="str">$1$2$3$2</span>`
