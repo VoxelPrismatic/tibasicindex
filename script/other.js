8b09d0d92b9e3edcda23f5a35fda39736787dfa8
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 10:19:19 -0600

    Update other.js

--- script/other.js
+++ script/other.js
@@ -10,6 +10,31 @@ function btn(elem, id) {
 }
 function highlight(phrase) {
     find("page").innerHTML = find("page").innerHTML.replace(RegExp(sec, "gm"), `<div class="find">${sec}</div>`);
-    find("linky").href = "&"+phrase;
-    find("linky").click();
+    url("&"+phrase);
+}
+function url(thing) {
+    href = find("url").innerHTML
+    var page = "";
+    var jump = "";
+    var find = "";
+    try {
+        page = href.split("?")[1].split("#")[0].split("&")[0].split(" ")[0];
+    } catch(err) {
+    } try {
+        jump = href.split("#")[1].split("?")[0].split("&")[0].split(" ")[0];
+    } catch(err) {
+    } try {
+        find = href.split("&")[1].split("?")[0].split("#")[0].split(" ")[0];
+    } catch(err) {
+    }
+    if(thing.startsWith("&"))
+        find = thing;
+    if(thing.startsWith("?"))
+        page = thing;
+    if(thing.startsWith("#"))
+        jump = thing;
+    href = `https://VoxelPrismatic.github.io/prizmatic.docs/?${page}#${jump}`
+    if(find != "")
+        href += "&"+find;
+    find("url").innerHTML = `[ ${href} ]`;
 }
