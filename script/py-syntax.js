56f2ffd38ab0ddb6040ba46315f5ca84fd2a03db
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 07:45:16 -0600

    Update py-syntax.js

--- script/py-syntax.js
+++ script/py-syntax.js
@@ -18,23 +18,29 @@ ops = "()[]<>{}|.,:;&£*^%=/-@";
 
 py_regex = [
     [
-        /( *)def ([\w\d_]+)/gm, 
-        `$1<span class="kw">def</span> <span class="fn">$2</span>`
-    ], [
-        /( *)class ([\w\d_]+)/gm,
-        `$1<span class="kw">class</span> <span class="cls">$2</span>`
-    ], [
         /([fFrRuUbB]?)(['"])(.+?)$2/gm,
-        `<span class="str">$1$2$3$2</span>`
-    ], [
-        /([fFrRuUbB]?)('''|""")((.|\n)+)$2/gm,
         function(m, a, b, c) {
             var s = "";
-            for(var z = 0; z < c.length(); z++)
+            for(var z = 0; z < c.length; z++)
                 s += c[z] + "\u200b";
             s = s.slice(0, -1);
             return `<span class="str">${a}${b}${s}${b}</span>`;
         }
+    ], [
+        /([fFrRuUbB]?)('''|""")((.|\n)+)$2/gm,
+        function(m, a, b, c) {
+            var s = "";
+            for(var z = 0; z < c.length; z++)
+                s += c[z] + "\u200b";
+            s = s.slice(0, -1);
+            return `<span class="str">${a}${b}${s}${b}</span>`;
+        }
+    ], [
+        /^( *)def ([\w\d_]+)/gm, 
+        `$1<span class="kw">def</span> <span class="fn">$2</span>`
+    ], [
+        /^( *)class ([\w\d_]+)/gm,
+        `$1<span class="kw">class</span> <span class="cls">$2</span>`
     ], [
         /([\w\d_]+)([\(\[.])/gm,
         `<span class="fn">$1</span>$2`
