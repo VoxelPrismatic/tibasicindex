2c4c4ea81445188165807a65879a01c4783ef87d
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-06 09:34:03 -0600

    Update run.js

--- script/run.js
+++ script/run.js
@@ -2,28 +2,7 @@ dirs = grab_dirs("/prizmatic.docs/doc"); // ../doc doesn't actually work for som
 url = document.URL;
 if(url.includes("?")) {
     var thing = url.split("?")[1].split("#")[0];
-    if(thing.startsWith("./doc"))
-        thing = thing.slice(5);
-    if(thing.startsWith("/"))
-        thing = thing.slice(1);
-    if(thing.endsWith(".txt")) {
-        thing = thing.slice(0, -3);
-        thing = thing.replace(/\./g, "/");
-        thing += ".txt";
-    } else {
-        thing = thing.replace(/\./g, "/");
-        thing += ".txt";
-    }
-    var fil = "/prizmatic.docs/doc/"+thing;
-} else {
-    var fil = "/prizmatic.docs/doc/index.txt";
-} if (!(dirs.includes(fil))) {
-    fil += "index.txt";
-} try {
-    load(fil);
-} catch(err) {
-    find("page").innerHTML = `<div class="warn">404 ] File not found</div>`;
-}
+    maybeload(thing);
 if(url.includes("#")) {
     url = url.replace("##", "#")
     var sec = url.split("#")[1].split("?")[0];
