b25614638f6ec4ffe3a3ac013d6df72f55db3218
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-07 22:01:42 -0600

    Update md.js

--- script/md.js
+++ script/md.js
@@ -149,7 +149,7 @@ function mk_ul(st) {
     var str = "";
     var ul = [];
     for(var line of st.split("\n").slice(0, -1))
-        ul.push(trim(line.slice(3)));
+        ul.push(trim(line.slice(2)));
     str += "<ul>";
     for(var li of ul)
         str += `<li>${mark_page(li)}</li>`;
