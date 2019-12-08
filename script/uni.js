2bbb6f35b86b74235fca5a77905a877edfa8d4e6
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-08 12:43:19 -0600

    Update uni.js

--- script/uni.js
+++ script/uni.js
@@ -1,5 +1,8 @@
-var unitxt = read("/prizmatic.docs/script/uni.txt");
-var unimap = {};
-for(var line of unitxt.split("\n"))
-    unimap[line.split("\u0009")[1]] = line.split("\u0009")[0];
+function getuni() {
+    var unitxt = read("/prizmatic.docs/script/uni.txt");
+    var uni = {};
+    for(var line of unitxt.split("\n"))
+        uni[line.split("\u0009")[1]] = line.split("\u0009")[0];
+}
+var unimap = getuni();
 console.log(unimap);
