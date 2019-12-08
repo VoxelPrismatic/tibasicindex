15e84fd010758ab6f052e267be7a4ec0b3586df5
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-08 12:54:01 -0600

    Delete uni.js

--- script/uni.js
+++
@@ -1,8 +0,0 @@
-function getuni() {
-    var unitxt = read("/prizmatic.docs/script/uni.txt");
-    var uni = {};
-    for(var line of unitxt.split("\n"))
-        uni[line.split("\u0009")[1]] = line.split("\u0009")[0];
-    return uni;
-}
-var unimap = getuni();
