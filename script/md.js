b39610300cd5563d9596dc770f3ff96bba504006
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-08 11:52:05 -0600

    Update md.js

--- script/md.js
+++ script/md.js
@@ -6,10 +6,10 @@ var line_regex = [
     [/^ /gm, "\u200b \u200b"],
     
     [/\\x([A-Fa-f0-9]{2})/gm, "\\u{$1}"],
-    [/\\U([A-Fa-f0-9]{16})/gm, "\\u{$1}"],
+    [/\\U([A-Fa-f0-9]{8})/gm, "\\u{$1}"],
     [/\\u([A-Fa-f0-9]{4})/gm, "\\u{$1}"],
     [/\\N\{(.+?)\}/gm, function(m, p) {return "\\u{"+unimap[p.toUpperCase()]+"}";}],
-    [/\\(.)/gm, function(m, p1) {return `\\u{${p1.charCodeAt(0).toString(16)}}`;}],
+    [/\\([^u])/gm, function(m, p1) {return `\\u{${p1.charCodeAt(0).toString(16)}}`;}],
     
     [/^\#\] +(.+)$/gm, "<div class='head1'>#] $1</div></br>"],
     [/^\~\] +(.+)$/gm, "<div class='head2'>~] $1</div></br>"],
