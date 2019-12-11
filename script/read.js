37066e669c757d908be9e359d90c0f23a5c2a9f3
PRIZ ;] <45671764+VoxelPrismatic@users.noreply.github.com>
2019-12-10 21:52:54 -0600

    Update read.js

--- script/read.js
+++ script/read.js
@@ -11,11 +11,16 @@ function read(filename) {
 
 function readAsync(filename) {
     var f = new XMLHttpRequest()
+    var cur = findHtml("file");
     f.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200)
             find("file").innerHTML = f.responseText.replace(/  /gm, "\u200b \u200b \u200b");
     }
     f.open("GET", filename, true);
     f.send();
+    var trys = 0;
+    while(findHtml("file") == cur && trys < 5)
+        trys += 1;
+        setTimeout(() => {console.log("wait");}, 500);
     return findHtml("file");
 }
